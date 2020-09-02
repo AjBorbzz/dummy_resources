01d1eb2c0e94222d5c173e48919588b2315c0f8f
ajborbe <albert.john@iscale-solutions.com>
2020-09-02 11:13:06 +0800

    corrected spelling packages

--- resources/setup.py
+++ resources/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
         name="resources",
-        pakages=['resources'],
+        packages=['resources'],
         description="Resources dummy will delete later",
         version="0.1",
         url="https://github.com/AjBorbzz/dummy_resources.git",
